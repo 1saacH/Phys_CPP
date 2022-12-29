24aee57140421279e1e1416a8824f5e3c1a668db
TheKenDoll <58020437+TheKenDoll@users.noreply.github.com>
2022-12-29 13:45:55 -0700

    adding cpp file

---
+++ col_phys.cpp
@@ -0,0 +1,15 @@
+// c++ file for physics python module
+//
+
+#define PY_SSIZE_T_CLEAN
+//#include <Python.h>
+
+#include <iostream>
+
+int main() {
+
+
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
